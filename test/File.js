26454f8f6683ab5ea419470fbe4b2bf5f5ff4aa0
Lasse <lasse@haslev.no>
2017-07-26 09:33:04 +0200

    Changed url image from placehold.it to google

--- test/File.js
+++ test/File.js
@@ -11,14 +11,14 @@ let requestFile = null;
 test.before( t => {
     relativeFile = new File( 'test/mocks/image.jpg' );
     absoluteFile = new File( '/test.jpg' );
-    requestFile = new File( 'http://via.placeholder.com/350x150' );
+    requestFile = new File( 'https://www.google.no/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' );
 });
 
 test( 'it can get full path of file', t => {
 
     t.is( relativeFile.path(), process.cwd() + '/test/mocks/image.jpg' );
     t.is( absoluteFile.path(), '/test.jpg' );
-    t.is( requestFile.path(), 'http://via.placeholder.com/350x150' );
+    t.is( requestFile.path(), 'https://www.google.no/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png' );
 
 });
 
@@ -31,10 +31,10 @@ test( 'it can check if file exists', t => {
 test( 'it can guess the mime type', t => {
     t.is( relativeFile.mimeType(), 'image/jpeg' );
     t.is( absoluteFile.mimeType(), 'image/jpeg' );
-    t.is( requestFile.mimeType(), null );
+    t.is( requestFile.mimeType(), 'image/png' );
 
-    let file = new File( 'http://via.placeholder.com/350x150.jpg' );
-    t.is( file.mimeType(), 'image/jpeg' );
+    let file = new File( 'http://via.placeholder.com/350x150' );
+    t.is( file.mimeType(), null );
 
 } );
 // It can set base folder
