61d2766c73eb5bc1831fc5c6ab5d6049dc88fd9c
Lasse <lasse@haslev.no>
2017-07-26 10:17:52 +0200

    Added better check if the response is base 64

--- test/Encoder.js
+++ test/Encoder.js
@@ -6,8 +6,8 @@ let externalFilePath = 'https://www.google.no/images/branding/googlelogo/2x/goog
 
 // it can base64 encode a file from path
 test( 'it can base64 encode a file from path', t => {
-    let base64 = Encoder.encode( localFilePath );
-    t.is( base64, '"data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAAG1BMVEXMzMyWlpaqqqq3t7exsbGcnJy+vr6jo6PFxcUFpPI/AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAQUlEQVQ4jWNgGAWjgP6ASdncAEaiAhaGiACmFhCJLsMaIiDAEQEi0WXYEiMCOCJAJIY9KuYGTC0gknpuHwXDGwAA5fsIZw0iYWYAAAAASUVORK5CYII="' );
+    t.true( /^"data:image\/jpeg;base64,[A-Za-z0-9+/=]+="$/.test( Encoder.encode( localFilePath ) ) );
+    t.true( /^"data:image\/png;base64,[A-Za-z0-9+/=]+="$/.test( Encoder.encode( externalFilePath ) ) );
 } );
 
 // it takes a absolute or relative path or url to base64 encode file
